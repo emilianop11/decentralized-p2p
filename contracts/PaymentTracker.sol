d3adead480adbb62fdab025183af5afd1412c6e9
Emiliano Pelliccioni <emilianop11@gmail.com>
2023-02-13 21:29:16 -0300

    rename operation to transaction

--- contracts/PaymentTracker.sol
+++ contracts/PaymentTracker.sol
@@ -7,21 +7,21 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 // import "hardhat/console.sol";
 
 contract PaymentTracker {
-    enum OperationType{ SELL_CRYPTO, BUY_CRYPTO }
+    enum TransactionType{ SELL_CRYPTO, BUY_CRYPTO }
 
     struct Offer {
         uint createdAt;
         address createdBy;
         uint256 offerId;
-        OperationType opType;
+        TransactionType transactionType;
         uint256 maxAmount;
         uint256 minAmount;
         string paymentMethod;
     }
 
-    struct Operation {
+    struct Transaction {
         uint createdAt;
-        uint256 operationId;
+        uint256 transactionId;
         uint256 offerId;
         address sender;
         address receiver;
@@ -42,15 +42,15 @@ contract PaymentTracker {
     }
 
     address public owner;
-    mapping(uint256 => Operation) _operations;
+    mapping(uint256 => Transaction) _transactions;
     mapping(uint256 => Offer) _offers;
-    mapping(address => uint256[]) _addressesToOperations;
+    mapping(address => uint256[]) _addressesToTransactions;
     mapping(address => uint256[]) _addressesToOffers;
     address warrantyTokenAddress;
     enum State{ PENDING, FINISHED }
 
     using Counters for Counters.Counter;
-    Counters.Counter private _operationIdCounter;
+    Counters.Counter private _transactionIdCounter;
     Counters.Counter private _offerIdCounter;
 
     constructor(address _warrantyTokenAddress)  {
@@ -59,7 +59,7 @@ contract PaymentTracker {
     }
 
     function createOffer(
-        OperationType opType,
+        TransactionType transactionType,
         uint256 minAmount,
         uint256 maxAmount,
         string calldata paymentMethod
@@ -69,50 +69,50 @@ contract PaymentTracker {
         _offers[offerId].offerId = offerId;
         _offers[offerId].createdAt = block.timestamp;
         _offers[offerId].createdBy = msg.sender;
-        _offers[offerId].opType = opType;
+        _offers[offerId].transactionType = transactionType;
         _offers[offerId].maxAmount = maxAmount;
         _offers[offerId].minAmount = minAmount;
         _offers[offerId].paymentMethod = paymentMethod;
         _addressesToOffers[msg.sender].push(offerId);
     }
 
-    function createOperation(address _sender, address _receiver, uint256 _amount, string calldata _recipientData, uint256 offerId) public {
+    function createTransaction(address _sender, address _receiver, uint256 _amount, string calldata _recipientData, uint256 offerId) public {
         if(msg.sender != owner) {
             _sender = msg.sender;
         }
         require(offerId == 0 || _offers[offerId].createdBy == _sender  || _offers[offerId].createdBy == _receiver, "offer is not associated to this sender address");
 
         ERC20(warrantyTokenAddress).transferFrom(_sender, _receiver, _amount);
-        _operationIdCounter.increment();
-        uint256 operationId = _operationIdCounter.current();
-        _operations[operationId].operationId = operationId;
-        _operations[operationId].offerId = offerId;
-        _operations[operationId].sender = _sender;
-        _operations[operationId].receiver = _receiver;
-        _operations[operationId].amount = _amount;
-        _operations[operationId].state = State.PENDING;
-        _operations[operationId].recipientData = _recipientData;
-        _operations[operationId].createdAt = block.timestamp;
-        _addressesToOperations[_sender].push(operationId);
-        _addressesToOperations[_receiver].push(operationId);
+        _transactionIdCounter.increment();
+        uint256 transactionId = _transactionIdCounter.current();
+        _transactions[transactionId].transactionId = transactionId;
+        _transactions[transactionId].offerId = offerId;
+        _transactions[transactionId].sender = _sender;
+        _transactions[transactionId].receiver = _receiver;
+        _transactions[transactionId].amount = _amount;
+        _transactions[transactionId].state = State.PENDING;
+        _transactions[transactionId].recipientData = _recipientData;
+        _transactions[transactionId].createdAt = block.timestamp;
+        _addressesToTransactions[_sender].push(transactionId);
+        _addressesToTransactions[_receiver].push(transactionId);
 
     }
 
-    function completeOperation(uint256 _operationId) public {
-        Operation storage op = _operations[_operationId];
-        require(op.receiver == msg.sender, "operation can only be completed by receiver of the funds");
-        _operations[_operationId].state = State.FINISHED;
+    function completeTransaction(uint256 _transactionId) public {
+        Transaction storage transaction = _transactions[_transactionId];
+        require(transaction.receiver == msg.sender, "transaction can only be completed by receiver of the funds");
+        _transactions[_transactionId].state = State.FINISHED;
     }
 
-    function getOperationsForAddress() external view returns (Operation[] memory) {
-        uint256[] storage operationIds = _addressesToOperations[msg.sender];
-        Operation[] memory operations = new Operation[](operationIds.length);
-        for (uint i = 0; i < operationIds.length; i++) {
-            uint256 operationId = operationIds[i];
-            Operation storage cont = _operations[operationId];
-            operations[i] = cont;
+    function getTransactionsForAddress() external view returns (Transaction[] memory) {
+        uint256[] storage transactionIds = _addressesToTransactions[msg.sender];
+        Transaction[] memory transactions = new Transaction[](transactionIds.length);
+        for (uint i = 0; i < transactionIds.length; i++) {
+            uint256 transactionId = transactionIds[i];
+            Transaction storage transaction = _transactions[transactionId];
+            transactions[i] = transaction;
         }
-        return operations;
+        return transactions;
     }
 
     function getOffersForAddress() external view returns (Offer[] memory) {
--- test/PaymentTracker.js
+++ test/PaymentTracker.js
@@ -44,7 +44,7 @@ describe('Helper', function () {
           "createdBy": "0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
           "maxAmount": 1000,
           "minAmount": 1000,
-          "opType": 0,
+          "transactionType": 0,
           "offerId": 1,
           "paymentMethod": "banco nacion"
         }
@@ -62,7 +62,7 @@ describe('Helper', function () {
           "maxAmount": 1000,
           "minAmount": 1000,
           "offerId": 1,
-          "opType": 0,
+          "transactionType": 0,
           "paymentMethod": "banco nacion"
         },
         {
@@ -71,24 +71,24 @@ describe('Helper', function () {
           "maxAmount": 10000,
           "minAmount": 1000,
           "offerId": 2,
-          "opType": 1,
+          "transactionType": 1,
           "paymentMethod": "banco nacion"
         }
       ])
 
 
-      await expect(paymentsContract.connect(wallet3).createOperation(wallet3.address, wallet4.address, 100, "anything", 1)).to.be.revertedWith("offer is not associated to this sender address");
+      await expect(paymentsContract.connect(wallet3).createTransaction(wallet3.address, wallet4.address, 100, "anything", 1)).to.be.revertedWith("offer is not associated to this sender address");
 
-      await paymentsContract.connect(wallet1).createOperation(wallet1.address, wallet4.address, 100, "anything", 1);
-      await paymentsContract.connect(wallet1).createOperation(wallet1.address, wallet2.address, 100, "anything", 1);
+      await paymentsContract.connect(wallet1).createTransaction(wallet1.address, wallet4.address, 100, "anything", 1);
+      await paymentsContract.connect(wallet1).createTransaction(wallet1.address, wallet2.address, 100, "anything", 1);
 
 
-      const opsForAddress1 = await paymentsContract.connect(wallet1).getOperationsForAddress();
+      const opsForAddress1 = await paymentsContract.connect(wallet1).getTransactionsForAddress();
       const parsedOpsForAddres1 = ParseSolidityStruct(opsForAddress1)
       expect(parsedOpsForAddres1).to.eql([
         {
         "createdAt": parsedOpsForAddres1[0].createdAt,
-        "operationId":1,
+        "transactionId":1,
         "sender":"0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
         "receiver":"0x15d34AAf54267DB7D7c367839AAf71A00a2C6A65",
         "amount":100,
@@ -99,7 +99,7 @@ describe('Helper', function () {
         "associatedChat":[{"_hex":"0x00","_isBigNumber":true},"0x0000000000000000000000000000000000000000"]},
         {
           "createdAt": parsedOpsForAddres1[1].createdAt,
-          "operationId":2,
+          "transactionId":2,
           "sender":"0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
           "receiver":"0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC",
           "amount":100,
@@ -113,25 +113,25 @@ describe('Helper', function () {
     })
   });
   
-  describe('operations', function () {
-    it('should check transfers when operation was not created created by owner', async function () {
+  describe('transactions', function () {
+    it('should check transfers when transaction was not created created by owner', async function () {
 
       expect(await anyToken.balanceOf(wallet1.address)).to.equal(1000);
       expect(await anyToken.balanceOf(wallet2.address)).to.equal(1000);
       expect(await anyToken.balanceOf(wallet3.address)).to.equal(1000);
       // this is passing wallet3 as sender. we need to check that its not taken into consideration
-      await paymentsContract.connect(wallet1).createOperation(wallet3.address, wallet2.address, 100, "anything", 0);
+      await paymentsContract.connect(wallet1).createTransaction(wallet3.address, wallet2.address, 100, "anything", 0);
       expect(await anyToken.balanceOf(wallet1.address)).to.equal(900);
       expect(await anyToken.balanceOf(wallet2.address)).to.equal(1100);
       expect(await anyToken.balanceOf(wallet3.address)).to.equal(1000);
       // above we can effectively see that the amount was discounted from wallet1
 
-      const opsForAddress1 = await paymentsContract.connect(wallet1).getOperationsForAddress();
+      const opsForAddress1 = await paymentsContract.connect(wallet1).getTransactionsForAddress();
       const parsedOpsForAddres1 = ParseSolidityStruct(opsForAddress1)
       expect(parsedOpsForAddres1).to.eql([
         {
         "createdAt": parsedOpsForAddres1[0].createdAt,
-        "operationId":1,
+        "transactionId":1,
         "sender":"0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
         "receiver":"0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC",
         "amount":100,
@@ -144,10 +144,10 @@ describe('Helper', function () {
       expect(parsedOpsForAddres1[0].sender).to.equal(wallet1.address)
       expect(parsedOpsForAddres1[0].receiver).to.equal(wallet2.address)
       expect(parsedOpsForAddres1[0].state).to.equal(0)
-      const opsForAddress2 = await paymentsContract.connect(wallet2).getOperationsForAddress();
+      const opsForAddress2 = await paymentsContract.connect(wallet2).getTransactionsForAddress();
       const parsedOpsForAddres2 = ParseSolidityStruct(opsForAddress2)
       expect(parsedOpsForAddres2).to.eql([
-        {"operationId":1,
+        {"transactionId":1,
         "createdAt": parsedOpsForAddres2[0].createdAt,
         "sender":"0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
         "receiver":"0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC",
@@ -161,29 +161,29 @@ describe('Helper', function () {
       expect(parsedOpsForAddres2[0].receiver).to.equal(wallet2.address)
       expect(parsedOpsForAddres2[0].state).to.equal(0)
         
-      await expect(paymentsContract.connect(wallet1).completeOperation(1)).to.be.revertedWith("operation can only be completed by receiver of the funds");
-      const opsForAddress3 = await paymentsContract.connect(wallet1).getOperationsForAddress();
+      await expect(paymentsContract.connect(wallet1).completeTransaction(1)).to.be.revertedWith("transaction can only be completed by receiver of the funds");
+      const opsForAddress3 = await paymentsContract.connect(wallet1).getTransactionsForAddress();
       const parsedOpsForAddres3 = ParseSolidityStruct(opsForAddress3)
       expect(parsedOpsForAddres3[0].state).to.equal(0)
 
-      await paymentsContract.connect(wallet2).completeOperation(1)
-      const opsForAddress4 = await paymentsContract.connect(wallet1).getOperationsForAddress();
+      await paymentsContract.connect(wallet2).completeTransaction(1)
+      const opsForAddress4 = await paymentsContract.connect(wallet1).getTransactionsForAddress();
       const parsedOpsForAddres4 = ParseSolidityStruct(opsForAddress4)
       expect(parsedOpsForAddres4[0].state).to.equal(1)
     });
 
-    it('should check transfers when operation was created by owner', async function () {
+    it('should check transfers when transaction was created by owner', async function () {
 
       expect(await anyToken.balanceOf(wallet1.address)).to.equal(1000);
       expect(await anyToken.balanceOf(wallet2.address)).to.equal(1000);
-      await paymentsContract.connect(owner).createOperation(wallet1.address, wallet2.address, 100, "anything", 0);
+      await paymentsContract.connect(owner).createTransaction(wallet1.address, wallet2.address, 100, "anything", 0);
       expect(await anyToken.balanceOf(wallet1.address)).to.equal(900);
       expect(await anyToken.balanceOf(wallet2.address)).to.equal(1100);
 
-      const opsForAddress1 = await paymentsContract.connect(wallet1).getOperationsForAddress();
+      const opsForAddress1 = await paymentsContract.connect(wallet1).getTransactionsForAddress();
       const parsedOpsForAddres1 = ParseSolidityStruct(opsForAddress1)
       expect(parsedOpsForAddres1).to.eql([
-        {"operationId":1,
+        {"transactionId":1,
         "createdAt": parsedOpsForAddres1[0].createdAt,
         "sender":"0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
         "receiver":"0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC",
@@ -197,10 +197,10 @@ describe('Helper', function () {
       expect(parsedOpsForAddres1[0].sender).to.equal(wallet1.address)
       expect(parsedOpsForAddres1[0].receiver).to.equal(wallet2.address)
       expect(parsedOpsForAddres1[0].state).to.equal(0)
-      const opsForAddress2 = await paymentsContract.connect(wallet2).getOperationsForAddress();
+      const opsForAddress2 = await paymentsContract.connect(wallet2).getTransactionsForAddress();
       const parsedOpsForAddres2 = ParseSolidityStruct(opsForAddress2)
       expect(parsedOpsForAddres2).to.eql([
-        {"operationId":1,
+        {"transactionId":1,
         "createdAt": parsedOpsForAddres2[0].createdAt,
         "sender":"0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
         "receiver":"0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC",
@@ -214,13 +214,13 @@ describe('Helper', function () {
       expect(parsedOpsForAddres2[0].receiver).to.equal(wallet2.address)
       expect(parsedOpsForAddres2[0].state).to.equal(0)
         
-      await expect(paymentsContract.connect(wallet1).completeOperation(1)).to.be.revertedWith("operation can only be completed by receiver of the funds");
-      const opsForAddress3 = await paymentsContract.connect(wallet1).getOperationsForAddress();
+      await expect(paymentsContract.connect(wallet1).completeTransaction(1)).to.be.revertedWith("transaction can only be completed by receiver of the funds");
+      const opsForAddress3 = await paymentsContract.connect(wallet1).getTransactionsForAddress();
       const parsedOpsForAddres3 = ParseSolidityStruct(opsForAddress3)
       expect(parsedOpsForAddres3[0].state).to.equal(0)
 
-      await paymentsContract.connect(wallet2).completeOperation(1)
-      const opsForAddress4 = await paymentsContract.connect(wallet1).getOperationsForAddress();
+      await paymentsContract.connect(wallet2).completeTransaction(1)
+      const opsForAddress4 = await paymentsContract.connect(wallet1).getTransactionsForAddress();
       const parsedOpsForAddres4 = ParseSolidityStruct(opsForAddress4)
       expect(parsedOpsForAddres4[0].state).to.equal(1)
     });
