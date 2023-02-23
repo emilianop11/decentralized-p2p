485124bf2f44172ecb20d50073003622e90de3b1
Emiliano Pelliccioni <emilianop11@gmail.com>
2023-02-23 00:02:24 -0300

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,10 +1,10 @@
 //USDC 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174
 
 const main = async () => {
-  const helperContractFactory = await hre.ethers.getContractFactory('P2P');
-  const helperContract = await helperContractFactory.deploy("0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174");
-  await helperContract.deployed();
-  console.log('Contract Address:', helperContract.address);
+  const p2pContractFactory = await hre.ethers.getContractFactory('P2P');
+  const p2pContract = await p2pContractFactory.deploy("0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174");
+  await p2pContract.deployed();
+  console.log('Contract Address:', p2pContract.address);
 };
 
 const runMain = async () => {
